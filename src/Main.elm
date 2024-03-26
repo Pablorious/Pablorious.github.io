3f616e9eecfc2f9f969f9811c9fbe9f6433f5160
Pablorious <pabloreboredosegovia@gmail.com>
2024-03-26 16:45:34 -0400

    okay done for now

--- src/Main.elm
+++ src/Main.elm
@@ -73,7 +73,7 @@ view model =
         ]
         <| column
             [ width (if model.width > model.height then 
-                    fill |> maximum  (max 768 <| 100 * model.vu)
+                    fill |> maximum  (max 768 <| 120 * model.vu)
                 else
                     fill)
             , centerX
