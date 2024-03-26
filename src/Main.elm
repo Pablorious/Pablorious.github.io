e67b984c5a87e6dd131d6d5a4509e2c2bc798408
Pablorious <pabloreboredosegovia@gmail.com>
2024-03-26 16:44:19 -0400

    uuugh

--- src/Main.elm
+++ src/Main.elm
@@ -73,7 +73,7 @@ view model =
         ]
         <| column
             [ width (if model.width > model.height then 
-                    fill |> maximum  (max 768 <| 30 * model.vu)
+                    fill |> maximum  (max 768 <| 100 * model.vu)
                 else
                     fill)
             , centerX
